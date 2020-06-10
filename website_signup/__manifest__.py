9f2ced59b5aac969f0cb850d9720b50f5dc9fbe5
kelvzxu <ceokelvin12@gmail.com>
2020-06-10 09:43:32 +0700

    change __manifest__.py

--- website_signup/__manifest__.py
+++ website_signup/__manifest__.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Kelvzxu (<https://kltech-intl.odoo.com/>)
+# Copyright(c): 2015-kltech-intl.
+# All Rights Reserved.
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#################################################################################
+
 {
-    'name': "website_signup",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+  "name"                 :  "Website Signup",
+  "summary"              :  "view your insert password at login and signup page",
+  "category"             :  "Website",
+  "version"              :  "1.0.0",
+  "author"               :  "kelvzxu",
+  "website"              : "https://kltech-intl.odoo.com",
+  "description"          :  "view your insert password at login and signup page",
+  "depends"              :  ['website'],
+  "data"                 :  ['views/auth_signup_login.xml'],
+  "images"               :  ['static/description/Banner.png'],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+  "pre_init_hook"        :  "pre_init_check",
 }
