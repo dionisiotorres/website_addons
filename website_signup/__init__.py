de327d7828718581f020dfd4231db52431647cd6
kelvzxu <ceokelvin12@gmail.com>
2020-06-10 11:30:48 +0700

    create models website_signup, views website_signup_template and modify ir.model.access.cv

--- website_signup/__init__.py
+++ website_signup/__init__.py
@@ -3,10 +3,6 @@
 # Author      : Kelvzxu (<https://kltech-intl.odoo.com/>)
 # Copyright(c): 2015-kltech-intl.
 # All Rights Reserved.
-#
-#
-# This program is copyright property of the author mentioned above.
-# You can`t redistribute it and/or modify it.
 #################################################################################
 
 from . import models
--- website_signup/__manifest__.py
+++ website_signup/__manifest__.py
@@ -3,10 +3,6 @@
 # Author      : Kelvzxu (<https://kltech-intl.odoo.com/>)
 # Copyright(c): 2015-kltech-intl.
 # All Rights Reserved.
-#
-#
-# This program is copyright property of the author mentioned above.
-# You can`t redistribute it and/or modify it.
 #################################################################################
 
 {
@@ -17,8 +13,11 @@
   "author"               :  "kelvzxu",
   "website"              :  "https://kltech-intl.odoo.com",
   "description"          :  "configure signup page with dynamic fields for each website.",
-  "depends"              :  ['auth_signup','website'],
-  "data"                 :  ['views/auth_signup_login.xml'],
+  "depends"              :  ['auth_signup','website',],
+  "data"                 :  [
+                              "security/ir.model.access.csv",
+                              "views/website_signup_views.xml",
+                            ],
   "images"               :  ['static/description/Banner.png'],
   "application"          :  True,
   "installable"          :  True,
